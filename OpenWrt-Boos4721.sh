6a0685c2709d475bb6926be562d520bcbcaed136
atph2006 <105761411+atph2006@users.noreply.github.com>
2022-05-19 08:28:05 +0800

    Update OpenWrt-Boos4721.sh

--- OpenWrt-Boos4721.sh
+++ OpenWrt-Boos4721.sh
@@ -20,7 +20,7 @@ cat feeds.conf.default
 # 删除软件包
 #rm -rf ./package/lean/UnblockNeteaseMusic
 #rm -rf ./package/lean/UnblockNeteaseMusic-Go
-rm -rf .package/luci-app-clash
+#rm -rf .package/luci-app-clash
 
 # 添加第三方软件包
 #git clone https://github.com/gbaoye/openwrt-packages package/openwrt-packages
@@ -32,7 +32,7 @@ git clone https://github.com/kongfl888/luci-app-adguardhome package/luci-app-adg
 #git clone https://github.com/frainzy1477/luci-app-clash package/luci-app-clash
 #git clone https://github.com/hubbylei/luci-app-clash package/luci-app-clash
 #git clone https://github.com/110010110/luci-app-adguardhome package/luci-app-adguardhome
-git clone https://github.com/kenzok8/openwrt-packages/tree/master/luci-app-smartdns.git package/luci-app-smartdns
+#git clone https://github.com/kenzok8/openwrt-packages/tree/master/luci-app-smartdns.git package/luci-app-smartdns
 git clone https://github.com/immortalwrt/luci-app-unblockneteasemusic.git package/luci-app-unblockneteasemusic
 #git clone https://github.com/hubbylei/luci-app-clash package/luci-app-clash
 
@@ -41,8 +41,8 @@ git clone https://github.com/xiaorouji/openwrt-passwall.git package/openwrt-pass
 git clone -b luci https://github.com/xiaorouji/openwrt-passwall.git package/luci-app-passwall
 
 #passwall2
-git clone https://github.com/xiaorouji/openwrt-passwall.git package/openwrt-passwall
-git clone https://github.com/xiaorouji/openwrt-passwall2.git package/openwrt-passwall2
+#git clone https://github.com/xiaorouji/openwrt-passwall.git package/openwrt-passwall
+#git clone https://github.com/xiaorouji/openwrt-passwall2.git package/openwrt-passwall2
 
 
 # 下载自定义软件
@@ -52,13 +52,13 @@ git clone https://github.com/xiaorouji/openwrt-passwall2.git package/openwrt-pas
 # 替换更新插件
 #rm -rf package/openwrt-packages/luci-app-passwall && svn co https://github.com/Lienol/openwrt-package/trunk/lienol/luci-app-passwall package/openwrt-packages/luci-app-passwall
 #rm -rf package/openwrt-packages/luci-app-ssr-plus && svn co https://github.com/fw876/helloworld package/openwrt-packages/helloworld
-rm -rf package/openwrt-packages/adguardhome && svn co https://github.com/Lienol/openwrt/tree/dev-19.07/package/diy/adguardhome package/openwrt-packages/adguardhome
+#rm -rf package/openwrt-packages/adguardhome && svn co https://github.com/Lienol/openwrt/tree/dev-19.07/package/diy/adguardhome package/openwrt-packages/adguardhome
 rm -rf package/openwrt-packages/luci-app-adguardhome && svn co https://github.com/kongfl888/luci-app-adguardhome package/openwrt-packages/luci-app-adguardhome
 #rm -rf package/openwrt-packages/luci-app-clash && svn co https://github.com/hubbylei/luci-app-clash package/openwrt-packages/luci-app-clash
 
 # 添加passwall依赖库
-#git clone https://github.com/kenzok8/small package/small
-#svn co https://github.com/Lienol/openwrt-package/tree/master/package package/small
+git clone https://github.com/kenzok8/small package/small
+svn co https://github.com/Lienol/openwrt-package/tree/master/package package/small
 
 # 替换更新haproxy默认版本
 #rm -rf feeds/packages/net/haproxy && svn co https://github.com/Lienol/openwrt-packages/net/haproxy feeds/packages/net/haproxy
